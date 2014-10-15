8cc9e290dcdce3dc3f5b08244e7cc6d97cfb3847
AndreasKalstad <andy_5559@hotmail.com>
2014-10-15 12:13:24 +0200

    fix

--- core/src/draganddrop/BagTarget.java
+++ core/src/draganddrop/BagTarget.java
@@ -3,7 +3,6 @@ package draganddrop;
 import objects.Bag;
 import actors.BagActor;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Payload;
 import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Source;
 import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Target;
